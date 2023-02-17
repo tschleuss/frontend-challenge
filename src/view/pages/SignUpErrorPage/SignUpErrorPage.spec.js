3a6fe8e6df2548810a8eb2806617291934c52f40
Thyago Schleuss <tschleuss@gmail.com>
2023-02-17 14:55:01 -0600

    fix: fixing some test evaluations

--- src/view/pages/SignUpErrorPage/SignUpErrorPage.spec.js
+++ src/view/pages/SignUpErrorPage/SignUpErrorPage.spec.js
@@ -1,11 +1,17 @@
-import { screen } from "@testing-library/react"
+import { screen } from "@testing-library/react";
 import { renderUI } from "app/testing/testHelper";
-import { SignUpErrorPage } from "./SignUpErrorPage"
+import { SignUpErrorPage } from "./SignUpErrorPage";
 
 describe("SignUpErrorPage", () => {
-  it("should load and display page name", () => {
+  it("should load and render correctly", () => {
     renderUI(<SignUpErrorPage />);
 
-    expect(screen.getByText("SignUpErrorPage")).toBeVisible();
+    expect(
+      screen.getByRole("heading", { level: 1, name: /Error/i })
+    ).toBeVisible();
+    expect(
+      screen.getByText("Uh oh, something went wrong. Please try again later")
+    ).toBeVisible();
+    expect(screen.getByRole("button", { name: /Restart/i })).toBeVisible();
   });
 });
--- src/view/pages/SignUpSuccessPage/SignUpSuccessPage.spec.js
+++ src/view/pages/SignUpSuccessPage/SignUpSuccessPage.spec.js
@@ -3,9 +3,15 @@ import { renderUI } from "app/testing/testHelper";
 import { SignUpSuccessPage } from "./SignUpSuccessPage";
 
 describe("SignUpSuccessPage", () => {
-  it("should load and display page name", () => {
+  it("should load and render correctly", () => {
     renderUI(<SignUpSuccessPage />);
 
-    expect(screen.getByText("SignUpSuccessPage")).toBeVisible();
+    expect(
+      screen.getByRole("heading", { level: 1, name: /Success/i })
+    ).toBeVisible();
+    expect(
+      screen.getByText("You should receive a confirmation email soon.")
+    ).toBeVisible();
+    expect(screen.getByRole("button", { name: /Restart/i })).toBeVisible();
   });
 });
