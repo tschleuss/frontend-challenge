0eb52e738ed22d044b9331c7f66a149d6452f336
Thyago Schleuss <tschleuss@gmail.com>
2023-02-17 14:41:04 -0600

    feat: adding SignUp confirmation page

--- src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.jsx
+++ src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.jsx
@@ -1,3 +1,48 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles";
 
-export const SignUpConfirmationPage = () => <div>SignUpConfirmationPage</div>;
+export const SignUpConfirmationPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header>
+        <h1>Confirmation</h1>
+      </header>
+      <S.Form
+        onSubmit={(event) => {
+          event.preventDefault();
+          navigate("/success");
+        }}
+      >
+        <p>
+          <span>First Name:</span> John
+        </p>
+        <p>
+          <span>E-mail:</span> john@domain.com
+        </p>
+        <p>
+          <span>Password:</span> ******
+        </p>
+        <p>
+          <span>Favorite Color:</span> blue
+        </p>
+        <p>
+          <span>Terms and Conditions:</span> agreed
+        </p>
+        <S.ActionFooter>
+          <S.ReturnButton
+            type="button"
+            onClick={(event) => {
+              navigate("/more-info");
+            }}
+          >
+            Back
+          </S.ReturnButton>
+          <S.SubmitButton type="submit">Next</S.SubmitButton>
+        </S.ActionFooter>
+      </S.Form>
+    </>
+  );
+};
--- src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.spec.js
+++ src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.spec.js
@@ -1,11 +1,19 @@
-import { screen } from "@testing-library/react"
+import { screen } from "@testing-library/react";
 import { renderUI } from "app/testing/testHelper";
-import { SignUpConfirmationPage } from "./SignUpConfirmationPage"
+import { SignUpConfirmationPage } from "./SignUpConfirmationPage";
 
 describe("SignUpConfirmationPage", () => {
-  it("should load and display page name", () => {
+  it("should load and render correctly", () => {
     renderUI(<SignUpConfirmationPage />);
 
-    expect(screen.getByText("SignUpConfirmationPage")).toBeVisible();
+    expect(
+      screen.getByRole("heading", { level: 1, name: /Confirmation/i })
+    ).toBeVisible();
+    expect(screen.getByText("First Name:")).toBeVisible();
+    expect(screen.getByText("Password:")).toBeVisible();
+    expect(screen.getByText("Favorite Color:")).toBeVisible();
+    expect(screen.getByText("Terms and Conditions:")).toBeVisible();
+    expect(screen.getByRole("button", { name: /Back/i })).toBeVisible();
+    expect(screen.getByRole("button", { name: /Next/i })).toBeVisible();
   });
 });
