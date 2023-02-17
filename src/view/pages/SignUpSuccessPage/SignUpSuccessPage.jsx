1fea5437773f63075742a3a859f4b06e09d6651b
Thyago Schleuss <tschleuss@gmail.com>
2023-02-17 14:45:52 -0600

    feat: adding SignUp success

--- src/view/pages/SignUpSuccessPage/SignUpSuccessPage.jsx
+++ src/view/pages/SignUpSuccessPage/SignUpSuccessPage.jsx
@@ -1,3 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import * as S from "./styles";
 
-export const SignUpSuccessPage = () => <div>SignUpSuccessPage</div>
+export const SignUpSuccessPage = () => {
+  const navigate = useNavigate();
+
+  return (
+    <>
+      <header>
+        <h1>Success</h1>
+      </header>
+      <p>You should receive a confirmation email soon.</p>
+      <S.ActionFooter>
+        <S.RestartButton
+          type="button"
+          onClick={(event) => {
+            navigate("/");
+          }}
+        >
+          Restart
+        </S.RestartButton>
+      </S.ActionFooter>
+    </>
+  );
+};
