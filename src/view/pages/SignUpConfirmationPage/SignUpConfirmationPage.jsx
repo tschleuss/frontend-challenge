36bbfd967ae805b8468478dfa6eee5929d8cda2f
Thyago Schleuss <tschleuss@gmail.com>
2023-02-17 14:45:32 -0600

    fix: removing form from confirmation page

--- src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.jsx
+++ src/view/pages/SignUpConfirmationPage/SignUpConfirmationPage.jsx
@@ -10,39 +10,39 @@ export const SignUpConfirmationPage = () => {
       <header>
         <h1>Confirmation</h1>
       </header>
-      <S.Form
-        onSubmit={(event) => {
-          event.preventDefault();
-          navigate("/success");
-        }}
-      >
-        <p>
-          <span>First Name:</span> John
-        </p>
-        <p>
-          <span>E-mail:</span> john@domain.com
-        </p>
-        <p>
-          <span>Password:</span> ******
-        </p>
-        <p>
-          <span>Favorite Color:</span> blue
-        </p>
-        <p>
-          <span>Terms and Conditions:</span> agreed
-        </p>
-        <S.ActionFooter>
-          <S.ReturnButton
-            type="button"
-            onClick={(event) => {
-              navigate("/more-info");
-            }}
-          >
-            Back
-          </S.ReturnButton>
-          <S.SubmitButton type="submit">Next</S.SubmitButton>
-        </S.ActionFooter>
-      </S.Form>
+      <p>
+        <span>First Name:</span> John
+      </p>
+      <p>
+        <span>E-mail:</span> john@domain.com
+      </p>
+      <p>
+        <span>Password:</span> ******
+      </p>
+      <p>
+        <span>Favorite Color:</span> blue
+      </p>
+      <p>
+        <span>Terms and Conditions:</span> agreed
+      </p>
+      <S.ActionFooter>
+        <S.ReturnButton
+          type="button"
+          onClick={(event) => {
+            navigate("/more-info");
+          }}
+        >
+          Back
+        </S.ReturnButton>
+        <S.SubmitButton
+          type="button"
+          onClick={(event) => {
+            navigate("/success");
+          }}
+        >
+          Next
+        </S.SubmitButton>
+      </S.ActionFooter>
     </>
   );
 };
