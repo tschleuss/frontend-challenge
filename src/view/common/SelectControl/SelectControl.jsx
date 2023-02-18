d87eb31bc5e475f0d77315d48cc4191099bbc923
Thyago Schleuss <tschleuss@gmail.com>
2023-02-17 22:34:00 -0600

    feat: creating select control

---
+++ src/view/common/SelectControl/SelectControl.jsx
@@ -0,0 +1,23 @@
+import { forwardRef } from "react";
+import { ValidationMessage } from "../ValidationMessage";
+import * as S from "./styles";
+
+export const SelectControl = forwardRef(
+  ({ name, defaultValue, label, onChange, onBlur, errors = {}, children }, ref) => (
+    <S.Container>
+      <S.Label id={`id-${name}`}>{label}</S.Label>
+      <S.Select
+        name={name}
+        ref={ref}
+        onChange={onChange}
+        onBlur={onBlur}
+        defaultValue={defaultValue}
+        aria-labelledby={`id-${name}`}
+        aria-invalid={errors[name] ? "true" : "false"}
+      >
+        {children}
+      </S.Select>
+      {errors[name] && <ValidationMessage message="This field is required" />}
+    </S.Container>
+  )
+);
---
+++ src/view/common/SelectControl/styles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1rem;
+`;
+
+export const Label = styled.label`
+  margin-bottom: 0.5rem;
+`;
+
+export const Select = styled.select`
+  border-radius: 0.25rem;
+  border: 1px solid #ced4da;
+  color: #212529;
+  line-height: 1.5;
+  padding: 0.375rem 0.75rem;
+`;
