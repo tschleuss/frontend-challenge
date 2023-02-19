9c408dd1ad7f206d41eb6d3af243b2935fdd346a
Thyago Schleuss <tschleuss@gmail.com>
2023-02-19 15:16:14 -0600

    feat: creating loading component

---
+++ src/view/common/LoadingSpinner/LoadingSpinner.jsx
@@ -0,0 +1,18 @@
+import { colors } from "app/theme/tokens";
+
+import * as S from "./styles";
+
+export const LoadingSpinner = ({
+  color = colors.black,
+  size = 32,
+  thickness = 4,
+}) => (
+  <S.Container>
+    <S.RingWrapper color={color} size={size} thickness={thickness}>
+      <S.Ring></S.Ring>
+      <S.Ring></S.Ring>
+      <S.Ring></S.Ring>
+      <S.Ring></S.Ring>
+    </S.RingWrapper>
+  </S.Container>
+);
---
+++ src/view/common/LoadingSpinner/styles.js
@@ -0,0 +1,53 @@
+import styled, { css, keyframes } from "styled-components";
+
+const ringAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const Ring = styled.div`
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  border-radius: 50%;
+  animation: ${ringAnimation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  position: relative;
+  width: 100%;
+  height: 100%;
+
+  ${Ring}:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+
+  ${Ring}:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+
+  ${Ring}:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+`;
+
+export const RingWrapper = styled.div`
+  ${({ color, size, thickness }) => css`
+    position: relative;
+    width: ${size}px;
+    height: ${size}px;
+
+    ${Ring} {
+      width: ${size}px;
+      height: ${size}px;
+      border: ${thickness}px solid ${color};
+      border-color: ${color} transparent transparent transparent;
+    }
+  `}
+`;
